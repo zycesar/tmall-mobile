e26ceb10e4ad7b79509d92291f4cbb1d0a0b7307
zycesar <847596865@qq.com>
2016-10-27 16:46:58 +0800

    'update'

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-
+$(function(){
+    var header=$('.tm-m-h-content');
+    var module=$('.module-con');
+    var top=module.get(0).offsetTop;
+    var more=$('.header-more');
+    var nav=$('.nav');
+    var tableC=$('.table-cov');
+    window.onscroll=function(){
+        var stop=document.body.scrollTop||document.documentElement.scrollTop;
+        if(stop>top){
+            header.addClass('header-active');
+        }
+        if(stop<top){
+            header.removeClass('header-active');
+        }
+    }
+    more.on('touchstart',function(){
+       nav.addClass('active');
+    })
+    tableC.on('touchstart',function(){
+        nav.removeClass('active');
+    })
+    $('.tab-nav ul').on('touchstart','li',function(){
+        $('.tab-nav ul li').removeClass('aci')
+        $(this).toggleClass('aci');
+        
+    })
+})
